054831c3cda0f8be16ea5b9cb60a6f2ab975f077
Mulsi <58010034+Mulsi@users.noreply.github.com>
2019-11-20 23:07:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+express = require('express')
+app = express()
+path = require('path')
+metrics = require('./metrics')
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.set('port', 3000)
+app.set('views', __dirname + "/views")
+app.set('view engine', 'ejs');
+
+
+app.get(
+  '/hello/:Miha', 
+  (req, res) => res.render('hello.ejs', {name: req.params.name})
+  
+  )
+
+app.get('/metrics.json', (req, res) => {
+  metrics.get((err, data) => {
+    if(err) throw err
+    res.status(200).json(data)
+  })
+})
+  
+app.post('/', (req, res) => {
+    // POST
+})
+  
+app
+  .put('/', function (req, res) {
+  // PUT
+})
+  .delete('/', (req, res) => {    
+  // DELETE
+  })
+
+app.listen(
+  app.get('port'), 
+  () => console.log(`server listening on ${app.get('port')}`)
+)
